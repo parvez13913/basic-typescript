4d4f2e8731969560effd903a7402f2b3a6594f48
parvez13913 <parvezz13913@gmail.com>
2023-05-02 22:05:37 +0600

    tupple and arry

---
+++ src/arry.ts
@@ -0,0 +1,7 @@
+const names: string[] = ["Abul", "Babul", "Kabul"];
+
+const friendsId: number[] = [12, 13, 14];
+
+// tupple মুল্যত জোড়াই জোড়াই থাকে। যেমনঃ user প্রথমটা user id এবং দ্বিতীয়টা user name
+
+const user: [number, string] = [75, "Polash"];
