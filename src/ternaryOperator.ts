c8cf5db463ee4f247928249f3372524dbefaf9a2
parvez13913 <parvezz13913@gmail.com>
2023-05-05 20:51:39 +0600

    ternary operator and nullish coeslancing operator

---
+++ src/ternaryOperator.ts
@@ -0,0 +1,10 @@
+// ternary Operator
+const myAge: number = 22;
+const isAdult = myAge >= 22 ? "Yes" : "No";
+console.log(isAdult);
+
+// Nullish Coeslancing Operator=> Null And Undefined,Nullish Coeslancing Operator শুধুমাত্র Null এবং Undefined এর ক্ষেত্রে কাজ করে অন্য কোন ফলসি ভ্যালুর ক্ষেত্রে কাজ করেনা।
+
+const isUser = null;
+const fullUserName = isUser ?? "Guest";
+console.log(fullUserName);
