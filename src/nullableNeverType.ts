2c9312974c398680e7520ae63bea0855fc0552ad
parvez13913 <parvezz13913@gmail.com>
2023-05-05 20:35:33 +0600

    nullable Unknown and never Type

---
+++ src/nullableNeverType.ts
@@ -0,0 +1,37 @@
+// nullable Type
+const searchName = (value: string | null) => {
+  if (value === null) {
+    console.log("There is Nothing to search");
+  } else {
+    console.log("Searching....");
+  }
+};
+
+searchName(null);
+
+// Unknown Type
+
+const getMyCarSpeed = (speed: unknown) => {
+  if (typeof speed === "number") {
+    const convertedSpeed = (speed * 1000) / 3600;
+    console.log(`My Speed is ${convertedSpeed}`);
+  } else if (typeof speed === "string") {
+    const [value, unit] = speed.split(" ");
+    const convertedSpeed = (parseFloat(value) * 1000) / 3600;
+    console.log(`My Speed is ${convertedSpeed}`);
+  } else {
+    console.log("It is wrong Type");
+  }
+};
+
+getMyCarSpeed(10);
+getMyCarSpeed(false);
+getMyCarSpeed("20 kmh^-1");
+
+// Never Type=> Never কখনো কোন কিছু return করে না।
+
+function errorThrow(message: string): never {
+  throw new Error(message);
+}
+
+errorThrow("Error Khaichi");
