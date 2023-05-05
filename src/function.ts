2b9c0397aed0f6ee636359d523f1e069ad9a474b
parvez13913 <parvezz13913@gmail.com>
2023-05-05 11:47:37 +0600

    Spread-Rest-Default Parameters and Destreuctuing

--- src/function.ts
+++ src/function.ts
@@ -22,3 +22,39 @@ const person: {
     console.log(`My New Balance Is ${this.balance + money}`);
   },
 };
+
+// default value in function => default value সব সময় লাস্ট প্যারামিটারে দিতে হবে,প্রথম প্যারামিটারে দেওয়া আর না দেওয়া সমান।
+
+function defaultValueAddFunction(num1: number, num2: number = 10): number {
+  return num1 + num2;
+}
+// defaultValueAddFunction(10, 20); => default value ব্যাবহার না করতে চাইলে মান দিয়ে function কল করবো।
+
+// spread opearator Arry থেকে ইলিমেন্ট গুলোকে আলাদা করে ফেলে।
+
+const ourFriends = ["Hasem", "Kasem", "Josim"];
+const ourNewFriends = ["Asif", "Hasib", "Maruf"];
+
+ourFriends.push(...ourNewFriends);
+
+// console.log(ourFriends);
+
+// rest opearator ইলিমেন্ট গুলোকে নিয়ে একটা Arry তৈরি করে।
+
+const greedFriend = (...friends: string[]): void =>
+  friends.forEach((friend) => console.log(`Hi ${friend}`));
+
+greedFriend("Nasir", "Sabbir", "Taskin", "Hridey");
+
+// Arry and Object Destreucturing
+const friendss = ["Hasem", "Kasem", "Josim"];
+const [bestFriend] = friendss;
+
+const myBestFriend = {
+  bestFriendName: "Shakib",
+  age: 22,
+};
+
+const { age } = myBestFriend;
+
+// console.log(age, bestFriend);
